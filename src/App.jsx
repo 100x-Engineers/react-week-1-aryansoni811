d5ba95bcd5e911e109c18afd86d14752237841ed
Aryan Soni <arpitsoni811@gmail.com>
2023-11-07 04:19:50 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">This is check</h1>;123
+    </>
+  );
 }
 
 export default App;
